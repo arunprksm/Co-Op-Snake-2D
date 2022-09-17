8e8d051baa2ed14e55ef6e4fc5d06a2b5955014d
arunprksm <97307361+arunprksm@users.noreply.github.com>
2022-09-18 03:02:09 +0530

    Simple Update...

--- Co-Op Snake 2D/Assets/Scripts/Used Scripts/SnakeController.cs
+++ Co-Op Snake 2D/Assets/Scripts/Used Scripts/SnakeController.cs
@@ -8,7 +8,6 @@ public class SnakeController : MonoBehaviour
     string player1, player2;
 
     private Vector2 snakeDirection;
-    private Vector2 input;
     private bool up, left, down, right;
     private bool upArrow, leftArrow, downArrow, rightArrow;
     private List<Transform> snakeBodyExpand = new List<Transform>();
@@ -61,10 +60,6 @@ public class SnakeController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (input != Vector2.zero)
-        {
-            snakeDirection = input;
-        }
         MovementControl();
     }
 
@@ -97,7 +92,7 @@ public class SnakeController : MonoBehaviour
         {
             snakeDirection = Vector2.left;
         }
-        else if (right && input != Vector2.left)
+        else if (right && snakeDirection != Vector2.left)
         {
             snakeDirection = Vector2.right;
         }
